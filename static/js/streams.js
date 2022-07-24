76fd2454bb47650e36df6a0d2ef2176ddf096bfa
PrivateChpok <sazanov04@bk.ru>
2022-07-24 14:30:56 +0600

    second stream fix

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,6 +1,4 @@
-import AgoraRTC_N from '../assets/AgoraRTC_N-4.8.1'
-
-const Agora = AgoraRTC_N
+const AgoraRTC_N = require("../assets/AgoraRTC_N")
 
 const APP_ID = '5d111459f8184c65a10775d9aae86a04'
 const TOKEN = sessionStorage.getItem('token')
@@ -9,7 +7,7 @@ let UID = sessionStorage.getItem('UID')
 
 let NAME = sessionStorage.getItem('name')
 
-const client = Agora.createClient({mode:'rtc', codec:'vp8'})
+const client = AgoraRTC_N.createClient({mode:'rtc', codec:'vp8'})
 
 let localTracks = []
 let remoteUsers = {}
